ac5a214951466471f9b7d3ebe2d4e130f66b6a99
Christophe Bronner <christophe.bronner@videotron.ca>
2023-07-31 17:52:52 -0400

    Implemented VM ops

--- Nand2TetrisKit/Sources/VirtualMachine/Virtual Machine.swift
+++ Nand2TetrisKit/Sources/VirtualMachine/Virtual Machine.swift
@@ -19,18 +19,40 @@ public struct VirtualMachine {
 	public var a: UInt16 = 0
 
 	/// The value pointed to by ``a``
+	@inlinable @inline(__always)
 	public var m: UInt16 {
-		get {
-			self[ram: a]
-		}
-		set {
-			self[ram: a] = newValue
-		}
+		_read { yield ram[a] }
+		_modify { yield &ram[a] }
 	}
 
 	@inlinable @inline(__always)
 	public var instruction: Instruction {
-		Instruction(rawValue: self[rom: pc])
+		Instruction(rawValue: rom[pc])
+	}
+
+	@inlinable public mutating func cycle() {
+		flags = CycleFlags(instruction)
+		let result = UInt16(bitPattern: o)
+
+		if (instruction.lt && ng || instruction.eq && zr || instruction.gt && !ng) {
+			pc = a
+			flags.formUnion(.jmp)
+		} else {
+			pc += 1
+		}
+
+		if (instruction.m) {
+			m = result;
+			flags.formUnion(.m)
+		}
+		if (instruction.d) {
+			d = result;
+			flags.formUnion(.d)
+		}
+		if (instruction.a) {
+			a = result;
+			flags.formUnion(.a)
+		}
 	}
 
 	//MARK: - ALU
@@ -67,88 +89,101 @@ public struct VirtualMachine {
 		flags.contains(.ng)
 	}
 
+	@inlinable public static func alu(_ instruction: Instruction, with x: Int16, _ y: Int16) -> Int16 {
+		var tmp = CycleFlags.none
+		let result = alu(instruction, with: x, y, flags: &tmp)
+		return result
+	}
+
+	@inlinable public static func alu(_ instruction: Instruction, with x: Int16, _ y: Int16, flags: inout CycleFlags) -> Int16 {
+		var x = x
+		var y = y
+
+		// Compute X
+		if (instruction.zx) {
+			x = 0;
+		}
+		if (instruction.nx) {
+			x = ~x;
+		}
+
+		// Compute Y
+		if (instruction.zy) {
+			y = 0;
+		}
+		if (instruction.ny) {
+			y = ~y;
+		}
+
+		// Compute Out
+		var o = instruction.isAdd ? x+y : x&y;
+		if (instruction.no) {
+			o = ~o;
+		}
+
+		// Update flags
+		if o == 0 {
+			flags.formUnion(.zr)
+		}
+		if o < 0 {
+			flags.formUnion(.ng)
+		}
+
+		return o
+	}
+
 	//MARK: - ROM
 
 	/// The read-only memory of the computer
-	public let rom: VirtualMemory
+	public var rom: VirtualMemory
 
 	//MARK: - RAM
 
 	/// The read-write memory of the computer
-	public let ram: VirtualMemory
-
-	//MARK: Initialization
+	public var ram: VirtualMemory
 
 	public init() {
 		rom = .init()
 		ram = .init()
 	}
 
-	//MARK: Subscripts
+}
 
-	public subscript(rom address: UInt16) -> UInt16 {
-		_read {
-			_$observationRegistrar.access(self, keyPath: \.rom)
-			yield rom[Int(address)]
-		}
-		set {
-			_$observationRegistrar.withMutation(of: self, keyPath: \.rom) {
-				rom[Int(address)] = newValue
-			}
-		}
+public struct CycleFlags: RawRepresentable, OptionSet {
+
+	public var rawValue: UInt8
+
+	public init(rawValue: UInt8) {
+		self.rawValue = rawValue
 	}
 
-	public func rom(at address: UInt16) -> UInt16 {
-		self[rom: address]
+	@inlinable @inline(__always)
+	init(_ instruction: Instruction) {
+		let i = instruction.rawValue & 0x1000 >> 10
+		rawValue = UInt8(i)
 	}
 
-	public subscript(ram address: UInt16) -> UInt16 {
-		_read {
-			_$observationRegistrar.access(self, keyPath: \.ram)
-			yield ram[Int(address)]
-		}
-		set {
-			_$observationRegistrar.withMutation(of: self, keyPath: \.ram) {
-				ram[Int(address)] = newValue
-			}
-		}
-	}
+	public static let none = CycleFlags([])
 
-	public func ram(at address: UInt16) -> UInt16 {
-		self[ram: address]
-	}
+	/// Wether the ALU computed a 0
+	public static let zr = CycleFlags(rawValue: 0x1)
 
-	//MARK: Methods
+	/// Wether the ALU computed a negative value
+	public static let ng = CycleFlags(rawValue: 0x2)
 
-	public func step() {
-		// Fetch cycle information
-		let instruction = self.instruction
-		let x = self.x;
-		let y = self.y;
+	/// Wether RAM was read from
+	public static let i = CycleFlags(rawValue: 0x4)
 
-		// Compute the instruction's output
-		let (o, zr, ng) = VirtualMachine.alu(instruction, with: x, y)
-		self.zr = zr
-		self.ng = ng
+	/// Wether the address register was written to
+	public static let a = CycleFlags(rawValue: 0x8)
 
-		// Jump if necessary
-		let lt = { instruction.lt && ng }
-		let eq = { instruction.eq && zr }
-		let gt = { instruction.gt && !ng }
-		if (lt() || eq() || gt()) {
-			pc = a
-		}
+	/// Wether RAM was written to
+	public static let m = CycleFlags(rawValue: 0x10)
 
-		// Store output to the desired locations
-		if (instruction.m) {
-			m = o;
-		}
-		if (instruction.d) {
-			d = o;
-		}
-		if (instruction.a) {
-			a = o;
-		}
-	}
+	/// Wether the data register was written to
+	public static let d = CycleFlags(rawValue: 0x20)
+
+	/// Wether the instruction jumped
+	public static let jmp = CycleFlags(rawValue: 0x40)
 
 }
